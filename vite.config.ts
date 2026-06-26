92310bea9b3d9575a9b33fb283dfed180a094c76
ramter7 <ramter7@hotmail.com>
2026-06-26 05:27:18 +0200

    Rename dist to docs for GitHub Pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite"
 export default defineConfig({
   base: '/wedding/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   server: {
     port: 3000,
   },
