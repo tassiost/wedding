5304e124b7556997717104de9d17e30ad9da5da4
ramter7 <ramter7@hotmail.com>
2026-06-26 05:51:51 +0200

    Change base path to / for Render.com deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wedding/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'docs',
